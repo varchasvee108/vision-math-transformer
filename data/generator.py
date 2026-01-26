0e787a9cea5a9cf624caf611f44922a6522b4799
varchasvee <varchasvee108@gmail.com>
2026-01-26 10:51:09 +0530

    generator.py updated for generating samples

--- data/generator.py
+++ data/generator.py
@@ -15,7 +15,7 @@ def generate_single_image(
     img = Image.new("L", size=size, color=255)
     draw = ImageDraw.Draw(img)
 
-    a = (random.randint(0, 999),)
+    a = random.randint(0, 999)
     b = random.randint(0, 999)
 
     result = a + b
@@ -28,7 +28,7 @@ def generate_single_image(
     except:
         font = ImageFont.load_default()
 
-    rand_x = random.randit(10, 50)
+    rand_x = random.randint(10, 50)
     rand_y = random.randint(10, 50)
 
     draw.text((rand_x, rand_y), text=var_text, fill=0, font=font)
@@ -42,3 +42,59 @@ def generate_single_image(
         answer_tokens.append(processor.pad_id)
 
     return img, answer_tokens, result
+
+
+def generate_raw_data():
+    config = Config.load()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_samples", type=int, default=10000)
+    parser.add_argument("--seed", type=int, default=config.data.seed)
+    args = parser.parse_args()
+
+    random.seed(config.data.seed)
+
+    raw_dir = Path(config.data.raw_dir)
+    split_dir = Path(config.data.split_dir)
+
+    raw_dir.mkdir(parents=True, exist_ok=True)
+    split_dir.mkdir(parents=True, exist_ok=True)
+
+    dataset = []
+    image_size = tuple(config.data.image_size)
+
+    processor = VisionProcessor(image_size == image_size, vocab=config.data.vocab)
+
+    for i in range(args.num_samples):
+        img, tokens, result = generate_single_image(image_size, processor=processor)
+
+        file_name = f"math_{i:06d}.png"
+        file_path = raw_dir / file_name
+        img.save(file_path)
+
+        dataset.append(
+            {"image_path": str(file_path), "target_ids": tokens, "label_value": result}
+        )
+
+        if (i + 1) % 500 == 0:
+            print(f" {i+1} samples generated")
+
+    random.shuffle(dataset)
+
+    split_point = int(0.9 * len(dataset))
+
+    train_dataset = dataset[:split_point]
+    val_dataset = dataset[split_point:]
+
+    with open(split_dir / "train.json", "w") as f:
+        json.dump(train_dataset, f, indent=4)
+
+    with open(split_dir / "val.json", "w") as f:
+        json.dump(val_dataset, f, indent=4)
+
+    print(f"Generated {args.num_samples} samples")
+    print("Train/Val split : ", len(train_dataset), len(val_dataset))
+    print("Saved to: ", raw_dir, split_dir)
+
+
+if __name__ == "__main__":
+    generate_raw_data()
