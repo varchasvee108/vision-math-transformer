0566ee8a11c6632b54534090a734b75714683e66
varchasvee <varchasvee108@gmail.com>
2026-01-28 16:18:56 +0530

    infer.py added

---
+++ scripts/infer.py
@@ -0,0 +1,34 @@
+import torch
+from PIL import Image
+from core.config import Config
+from core.factory import build_inference_components
+
+
+def infer(
+    image_path: str,
+    config_path: str = "configs/base.toml",
+    model_weights="experiments/base/latest.pth",
+):
+    config = Config.load(config_path)
+    model, processor, device = build_inference_components(config=config)
+    model.load_state_dict(torch.load(model_weights, map_location=device))
+
+    img = Image.open(image_path).convert("L")
+    pixel_values = processor.process_image(img)
+
+    with torch.inference_mode():
+        generated_id = torch.tensor([[processor.sos_id]], device=device)
+
+        for _ in range(config.model.max_seq_len):
+            logits = model(pixel_values, generated_id)
+
+            next_token_logits = logits[:, -1, :]
+            next_token_id = torch.argmax(next_token_logits, dim=-1)
+
+            generated_id = torch.cat([generated_id, next_token_id], dim=-1)
+
+            if next_token_id.item() == processor.eos_id:
+                break
+
+    prediction = processor.decode(generated_id[0])
+    return prediction
