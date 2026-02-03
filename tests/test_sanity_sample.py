fd6b142573f29701c0b130545e0a2d6140b4761c
varchasvee <varchasvee108@gmail.com>
2026-02-03 11:17:50 +0530

    bug fix inference

--- tests/test_sanity_sample.py
+++ tests/test_sanity_sample.py
@@ -6,6 +6,7 @@ from core.factory import build_inference_components
 
 def test_sanity_sample():
     config = Config.load("configs/base.toml")
+
     model, processor, device = build_inference_components(config=config)
     model.eval()
 
